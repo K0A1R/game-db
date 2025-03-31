b51674595d528f10ee15482610e0d99260cd8919
Amrit Reddy <amrit_reddy@me.com>
2025-03-30 20:07:28 -0600

    Update Structure
    Add Header, Footer components.
    Add games-under-twenty folder and page.js route.

--- app/game-db/games-under-twenty/page.js
+++ app/game-db/games-under-twenty/page.js
@@ -5,7 +5,7 @@ export default function Page() {
   return (
     <main>
       <Header />
-      <h1>Favorites</h1>
+      <h1>Games under $20</h1>
       <Footer />
     </main>
   );
--- app/game-db/page.js
+++ app/game-db/page.js
@@ -1,10 +1,13 @@
 // Home Page
 import Link from "next/link";
+import Header from "./components/header";
+import Footer from "./components/footer";
 export default function Home() {
   return (
     <main>
-      <h1>Game Database</h1>
-      <Link href={"/game-db/favorites"}>My Favorites</Link>
+      <Header />
+      <h1>Welcome to GameDB</h1>
+      <Footer />
     </main>
   );
 }
